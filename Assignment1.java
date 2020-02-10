c456343b3365c0a33f536176081dd611485199cb
dking124 <60586385+dking124@users.noreply.github.com>
2020-02-09 19:21:54 -0500

    Add files via upload

---
+++ Assignment1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Assignment1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner keyboard = new Scanner(System.in);
+		int windSpeed;
+		int temperature;
+		String programmerName = "Darren King";
+		
+		System.out.println("Wind Chill Calculator\n");
+		System.out.println("What is the temperature in fahrenheit? (From -45-40F): ");
+		temperature = keyboard.nextInt();
+		System.out.println("What is the wind speed in MPH? (From 5-60 MPH): ");
+		windSpeed = keyboard.nextInt();
+		
+		double windChill = 35.74 + (.6215 * temperature) - (35.75 * Math.pow(windSpeed, .16)) + (.4275 * temperature * Math.pow(windSpeed, .16));
+		System.out.println("The wind chill is " + windChill);
+		System.out.println(programmerName);
+	}
+
+}
